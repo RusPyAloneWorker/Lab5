dadba7e72c3d50f93b973f667781cecd2fc643a1
RusLoneWorker <60140895+RusPyAloneWorker@users.noreply.github.com>
2023-12-16 00:34:09 +0300

    Update 5.c

--- 5.c
+++ 5.c
@@ -2,34 +2,50 @@
 #include <dirent.h>
 
 int main(int argc, char *argv[]) {
-    DIR *dir;
-    struct dirent *entry;
-    char* path;
-
     if (argc < 2) 
-        path = ".";
-    else
-    	path = argv[1];
+    {
+    	printf("Нет пути к директории");
+    	return 1;
+    }
 
-    // Открываем каталог
-    dir = opendir(path);
+    // Открываем каталоги
+    DIR * dir = opendir(argv[1]);
     if (dir == NULL) {
-        printf("Произошла ошибка при открытии директории: %s\n", path);
+        printf("Произошла ошибка при открытии директории: %s\n", argv[1]);
+        return 1;
+    }
+    
+    DIR * dir1 = opendir(".");
+    if (dir1 == NULL) {
+        printf("Произошла ошибка при открытии директории: %s\n", ".");
         return 1;
     }
 
-    // Выводим содержимое каталога
+    print(dir);
+    printf("------\n");
+    print(dir1);
+
+    // Закрываем каталог
+    if (closedir(dir) == -1)
+    {
+    	printf("Прoизошла ошибка при закрытии директории\n");
+    }
+    
+    if (closedir(dir1) == -1)
+    {
+    	printf("Прoизошла ошибка при закрытии директории\n");
+    }
+
+    return 0;
+}
+
+void print(DIR *dir)
+{
+	struct dirent *entry;
+	// Выводим содержимое каталога
     while ((entry = readdir(dir)) != NULL) {
         if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) // string comprasion. Одинаковы => 0
         	continue;
         printf("%s [%d]\n", entry->d_name, entry->d_reclen);
     }
-
-    // Закрываем каталог
-    if (closedir(dir) == -1)
-    {
-    	printf("Прoизошла ошибка при закрытии файла\n");
-    }
-
-    return 0;
 }
